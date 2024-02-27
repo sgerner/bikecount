5f2f7c8fac4a5f308fb66ce87100c8a703a2b6dc
Steven Gerner <steven@stevengerner.com>
2024-02-27 00:11:54 -0700

    Supabase Offline - Blocking Build

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
-import { createClient } from '@supabase/supabase-js';
+// import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
+// import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+// const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 
 export default supabase;
